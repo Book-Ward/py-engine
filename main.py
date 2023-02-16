4bb00d645894f97d8d348fdc893439487c943939
Iskren Aleksandrov <iskren.alexandrov@gmail.com>
2023-02-16 14:54:43 +0200

    Add caching to the endpoint

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, request
+from flask_caching import Cache
 import spacy
 import pytextrank
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 nlp = spacy.load("en_core_web_sm")
 nlp.add_pipe("textrank")
 
 @app.route('/process-reviews', methods=['POST'])
+@cache.cached(timeout=60*60, query_string=True)
 def process_text():
     text = request.json['text']
     doc = nlp(text)
